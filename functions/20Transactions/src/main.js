d6c642344dac7df7425ef175a6d89b3d9a9b04b1
GBuryska <144857718+GBuryska@users.noreply.github.com>
2025-04-06 12:38:25 -0500

    Update main.js

--- functions/20Transactions/src/main.js
+++ functions/20Transactions/src/main.js
@@ -2,6 +2,7 @@ import { Client, Databases } from 'node-appwrite';
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
+  log('hello');
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
   const client = new Client()
